c6e808abc7b5e732f304e1f156aa5bd9e9ec39d8
TroyMoses <mugabimoses07@gmail.com>
2024-09-27 13:32:14 +0800

    styles

--- finance_frontend/app/layout.tsx
+++ finance_frontend/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`inter.className, bg-gray-500 text-lg flex mx-auto justify-center items-center`}>{children}</body>
     </html>
   );
 }
