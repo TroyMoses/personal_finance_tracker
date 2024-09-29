6c4230088f307f73c1464fe4c6290b6e2429c553
TroyMoses <mugabimoses07@gmail.com>
2024-09-30 05:13:32 +0800

    dashboard

--- finance_frontend/app/(routes)/dashboard/page.jsx
+++ finance_frontend/app/(routes)/dashboard/page.jsx
@@ -86,7 +86,7 @@ function Dashboard() {
 
   return (
     <div className="p-8">
-      <h2 className="font-bold text-4xl">Hi, {userInfo?.username} 👋</h2>
+      <h2 className="font-bold text-4xl">Hi, <span className="capitalize">{userInfo?.username}</span> 👋</h2>
       <p className="text-gray-500">
         Here's what's happening with your money. Let's manage your expenses.
       </p>
