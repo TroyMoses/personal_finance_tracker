4882546ef463a57ad7edca744a32b943f21c4ae0
TroyMoses <mugabimoses07@gmail.com>
2024-09-29 23:15:59 +0800

    urls

--- finance_backend/transactions/urls.py
+++ finance_backend/transactions/urls.py
@@ -5,6 +5,9 @@ from .viewsets import TransactionViewSet, CategoryViewSet, signup, CustomAuthTok
 router = DefaultRouter()
 router.register('transactions', TransactionViewSet, basename='transaction')
 router.register('categories', CategoryViewSet, basename='category')
+router.register('budgets', BudgetViewSet, basename='budget')
+router.register('incomes', IncomeViewSet, basename='income')
+router.register('expenses', ExpenseViewSet, basename='expense')
 
 urlpatterns = [
     path('', include(router.urls)),
