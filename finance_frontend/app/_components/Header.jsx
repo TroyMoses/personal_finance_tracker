c899824dc50eea84e286c7ffc2ac01a0b34d443c
TroyMoses <mugabimoses07@gmail.com>
2024-09-29 19:49:50 +0800

    header

--- finance_frontend/app/_components/Header.jsx
+++ finance_frontend/app/_components/Header.jsx
@@ -1,20 +1,18 @@
 "use client";
+
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { useUser, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+
 function Header() {
-  const { user, isSignedIn } = useUser();
+
   return (
     <div className="p-5 flex justify-between items-center border shadow-sm">
       <div className="flex flex-row items-center">
         <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
         <span className="text-blue-800  font-bold text-xl">FinanSmart</span>
       </div>
-      {isSignedIn ? (
-        <UserButton />
-      ) : (
         <div className="flex gap-3  items-center">
           <Link href={"/dashboard"}>
             <Button variant="outline" className="rounded-full">
@@ -25,7 +23,6 @@ function Header() {
             <Button className="rounded-full">Get Started</Button>
           </Link>
         </div>
-      )}
     </div>
   );
 }
